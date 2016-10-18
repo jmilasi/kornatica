4e645674f1c43c5d479920acd8fa8419dcee4ffd
pavlek <pavlek1817@gmail.com>
2016-10-18 22:29:54 +0200

    Simple version of Romms, Translation, Language and InstanceType model are written

--- Kornatica/models.py
+++ Kornatica/models.py
@@ -2,7 +2,35 @@ from django.db import models
 
 
 # Create your models here.
-class Rooms(models.Model):
+class Language(models.Model):
     Name = models.CharField(max_length=100)
-    Description = models.CharField(max_length=500)
+    Abbr = models.CharField(max_length=10)
+
+
+class InstanceType(models.Model):
+    Name = models.CharField(max_length=100)
+
+
+class Translation(models.Model):
+    Value = models.CharField(max_length=1000)
+    Type = models.ForeignKey(InstanceType, on_delete=models.CASCADE)
+    LanguageId = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+
+class Rooms(models.Model):
+    NameTranslationId = models.ForeignKey(Translation, default=0, null=False, on_delete=models.CASCADE, related_name='Name')
+    DescriptionTranslationId = models.ForeignKey(Translation, default=0, null=False, on_delete=models.CASCADE, related_name='Description')
+    #LocationX = models.CharField(max_length=100)
+    #LocationY = models.CharField(max_length=100)
+    DistanceFromSee = models.IntegerField(default=0)
+    DistanceFromCenter = models.IntegerField(default=0)
+    NumberOfSeats = models.IntegerField(default=0, null=False)
+    Pets = models.BooleanField(default=0, null=False)
+    Parking = models.BooleanField(default=0, null=False)
+    TV = models.BooleanField(default=0, null=False)
+    Satellite = models.BooleanField(default=0, null=False)
+    WiFi = models.BooleanField(default=0, null=False)
+
+
+
 
